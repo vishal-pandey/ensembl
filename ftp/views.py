09411aa856774e67a38af35efee4cabe5e5ba2c3
vishal-pandey <contact@vishalpandey.xyz>
2019-03-20 17:01:13 +0530

    Using split instead of index

--- ftp/views.py
+++ ftp/views.py
@@ -17,22 +17,23 @@ def index(request):
 
 	output = []
 	for x in data:
+		y = x.split()
 		connector = '/'
 		if request.GET['path'] == '/' or request.GET['path'][-1] == '/':
 			connector = ''
 		vdict = {}
 		if x[0] == '-':
-			vdict['url'] = 'ftp://ftp.ensembl.org/pub/release-95'+str(request.GET['path'])+connector+str(x[56:])
+			vdict['url'] = 'ftp://ftp.ensembl.org/pub/release-95'+str(request.GET['path'])+connector+str(y[8])
 			vdict['type'] = 'file'
 			files = files+1
 		elif x[0] == 'd':
-			vdict['url'] = 'https://ensembl.vishalpandey.xyz/?path='+str(request.GET['path'])+connector+str(x[56:])
+			vdict['url'] = 'https://ensembl.vishalpandey.xyz/?path='+str(request.GET['path'])+connector+str(y[8])
 			vdict['type'] = 'dir'
 			dirs = dirs+1
-	  
-		vdict['size'] = int(x[28:42])
-		vdict['date_modified'] = x[42:56]
-		vdict['name'] = x[56:]
+		vdict['size'] = int(y[4])
+		vdict['date_modified'] = y[5]+" "+y[6]+" "+y[7]
+		vdict['name'] = str(y[8])
+		
 		output.append(vdict)
 
 	result = {}
