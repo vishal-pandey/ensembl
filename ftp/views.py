5c0f1d990cdae63893e61ff680474883020f53b9
vishal-pandey <contact@vishalpandey.xyz>
2019-03-20 13:54:13 +0530

    Removed trailing /

--- ftp/views.py
+++ ftp/views.py
@@ -12,22 +12,32 @@ def index(request):
 	data = []
 	ftp.dir(data.append)
 
+	files = 0
+	dirs = 0
+
 	output = []
 	for x in data:
 		connector = '/'
-		if request.GET['path'] == '/':
+		if request.GET['path'] == '/' or request.GET['path'][-1]:
 			connector = ''
 		vdict = {}
 		if x[0] == '-':
 			vdict['url'] = 'ftp://ftp.ensembl.org/pub/release-95'+str(request.GET['path'])+connector+str(x[56:])
 			vdict['type'] = 'file'
+			files = files+1
 		elif x[0] == 'd':
 			vdict['url'] = 'https://ensembl.vishalpandey.xyz/?path='+str(request.GET['path'])+connector+str(x[56:])
 			vdict['type'] = 'dir'
+			dirs = dirs+1
 	  
 		vdict['size'] = int(x[28:42])
 		vdict['date_modified'] = x[42:56]
 		vdict['name'] = x[56:]
 		output.append(vdict)
 
-	return HttpResponse(json.dumps(output))
+	result = {}
+	result['self'] = 'https://ensembl.vishalpandey.xyz/?path='+str(request.GET['path'])
+	result['files'] = files
+	result['dirs'] = dirs
+	result['data'] = output
+	return HttpResponse(json.dumps(result))
